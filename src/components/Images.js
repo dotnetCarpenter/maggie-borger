a1de536332ffd5d4eff653c3a15aafa32846dd17
dotnetCarpenter <jon.ronnenberg@gmail.com>
2024-06-19 14:21:22 +0200

    create image import utility

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,8 +1,49 @@
+import image0 from "../images//borger-survival.jpg";
+import image1 from "../images//borger-thecomment.jpg";
+import image2 from "../images//bottle brush 1.jpg";
+import image3 from "../images//broken.jpg";
+import image4 from "../images//dawsib.jpg";
+import image5 from "../images//echidna.jpg";
+import image6 from "../images//img-20231022-WA0006.jpg";
+import image7 from "../images//img-20231108-WA0004.jpg";
+import image8 from "../images//img-20231108-WA0009.jpg";
+import image9 from "../images//img_0558.jpg";
+import image10 from "../images//img_2830.jpg";
+import image11 from "../images//img_3359.jpg";
+import image12 from "../images//img_4196.jpg";
+import image13 from "../images//img_4567 1.jpg";
+import image14 from "../images//img_4568 1.jpg";
+import image15 from "../images//img_4573.jpg";
+import image16 from "../images//img_4576.jpg";
+import image17 from "../images//img_5290.jpg";
+import image18 from "../images//img_9512 1.jpg";
+import image19 from "../images//liferecline.jpg";
+import image20 from "../images//sea gift.jpg";
+import image21 from "../images//seal.jpg";
+import image22 from "../images//wheel.jpg";
+
 export default [
-	"borger-survival",
-	"borger-thecomment",
-	"bottle brush 1",
-	"broken",
-	"dawsib",
-	"echidna",
-]
+  image0,
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19,
+  image20,
+  image21,
+  image22,
+];
