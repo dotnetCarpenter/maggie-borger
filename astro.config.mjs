e69d814e05d01428ba5282383fbca778950e72b4
dotnetCarpenter <jon.ronnenberg@gmail.com>
2024-06-23 03:37:55 +0200

    change site tot work on maggie-borger.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://dotnetcarpenter.github.io',
-	base: 'maggie-borger',
+	site: 'https://maggie-borger.com',
 });
