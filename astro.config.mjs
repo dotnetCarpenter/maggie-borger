7f317aa1341a7247657842a3b8672c6bfaffebfc
dotnetCarpenter <jon.ronnenberg@gmail.com>
2024-06-29 17:42:31 +0200

    beautify code

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://maggie-borger.com'
+    site: 'https://maggie-borger.com',
+    build: {
+        inlineStylesheets: 'never'
+    }
 });
